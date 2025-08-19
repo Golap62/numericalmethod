02fc89c7fd2851d44b816276d7b2a15a42d78c07
Golap62 <bk1123708@gmail.com>
2025-08-19 23:30:09 +0600

    Add files via upload

---
+++ media.py
@@ -0,0 +1,17 @@
+# Program to check whether a number is prime
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):  # check divisors up to sqrt(num)
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime")
+    else:
+        print("Not Prime")
