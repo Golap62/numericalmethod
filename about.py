016755c8a9f1e16ac997dfaa192a35f2c020fe20
Golap62 <bk1123708@gmail.com>
2025-08-19 23:28:43 +0600

    Add files via upload

---
+++ about.py
@@ -0,0 +1,9 @@
+# Program to find sum of first n natural numbers
+
+n = int(input("Enter a number: "))
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum of first", n, "natural numbers is:", total)
